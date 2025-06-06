03bccf985da96778b51cbcecb662adb59d1d4656
AndreHilario <andre18hilario@gmail.com>
2025-06-06 09:12:33 -0300

    fix: seed

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -5,20 +5,12 @@ use Illuminate\Support\Facades\DB;
 
 class CategoriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
         DB::table('categories')->insert([
             ['name' => 'Eletrônicos', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Roupas', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Alimentos', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Livros', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Eletrodomésticos', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Brinquedos', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Esportes', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Móveis', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Beleza', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Outros', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
