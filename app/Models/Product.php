182d4e01d540f8c248682ba5f6313d60976fa39d
AndreHilario <andre18hilario@gmail.com>
2025-06-05 21:29:10 -0300

    feat: add base models

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function prices()
+    {
+        return $this->hasMany(ProductPrice::class);
+    }
+    
+    public function latestPrice()
+    {
+        return $this->hasOne(ProductPrice::class)->latest();
+    }
+}
