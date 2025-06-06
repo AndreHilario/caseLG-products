109e8f9dc9b252bb49a9d5957e766032c129f75a
AndreHilario <andre18hilario@gmail.com>
2025-06-05 22:49:31 -0300

    feat: add first commit to front-spa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
 require('./bootstrap');
+window.Vue = require('vue');
+Vue.component('products-component', require('./components/ProductsComponent.vue').default);
+const app = new Vue({ el: '#app' });
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('products', 'ProductController');
+Route::apiResource('products', 'Products\ProductController');
